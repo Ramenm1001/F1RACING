7aaeb7bd6f3a5b9ca39309cd413a1062a193fc1c
Роббо <gonozovdenis2010@gmail.co>
2025-05-03 15:04:32 +1000

    racecar

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
 
 
-win = pygame.display.set_mode((500, 500))
+win = pygame.display.set_mode((750, 500))
+
+
+car = pygame.image.load("sprites/yellow_car.png").convert_alpha()
+car = pygame.transform.scale(car, (50, 25))
+
 
 
 run = True
@@ -11,7 +16,9 @@ while run:
         if eve.type == pygame.QUIT:
             run = False
 
-    win.fill((0, 0, 0))
+    win.fill((0, 0, 77))
+
+    win.blit(car, (50, 50))
 
     #for some in island:
     #    some.update()
